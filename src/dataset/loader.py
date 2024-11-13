c71b77ca1f80c8801f333a457a63cd80b0c3a8b6
Mamiglia <matteo.migliarini.01@gmail.com>
2024-11-13 15:32:37 +0100

    add dataset loader for MNIST with DataLoader support

---
+++ src/dataset/loader.py
@@ -0,0 +1,26 @@
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, random_split
+
+
+def load_dataset(batch_size = 32, num_workers = 4, train_size = 0.8):
+        # Define transformations for the training and validation sets
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
+    # Load the MNIST dataset
+    dataset = datasets.MNIST(root='data', train=True, download=True, transform=transform)
+    test_dataset = datasets.MNIST(root='data', train=False, download=True, transform=transform)
+
+    # Split the dataset into training and validation sets
+    train_size = int(train_size * len(dataset))
+    val_size = len(dataset) - train_size
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+    # Create DataLoaders for training, validation, and test sets
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    return train_loader, val_loader, test_loader
